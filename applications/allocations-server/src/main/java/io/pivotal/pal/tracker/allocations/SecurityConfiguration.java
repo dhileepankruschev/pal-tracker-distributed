78dcb7e10051a5d18727c045e84d2401d05e3af1
dhileepankruschev <dhileepank@gmail.com>
2020-05-02 19:22:22 +0530

    Securing a Distributed System

--- applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/SecurityConfiguration.java
+++ applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/SecurityConfiguration.java
@@ -1,3 +1,4 @@
+/*
 package io.pivotal.pal.tracker.allocations;
 
 import org.springframework.context.annotation.Configuration;
@@ -19,3 +20,4 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
 }
+*/
